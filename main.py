d1a8e2f996a126416ea9e7e6c9448a14e636912c
Ketan Chowdhury <ketanchowdhury@protonmail.com>
2023-03-13 20:08:24 +0530

    Coded main.py to create a scatterplot and barplot at the same time.

--- main.py
+++ main.py
@@ -11,22 +11,25 @@ hashtag = "#flutter"
 
 load_dotenv()
 
-print("version", version)
-print("Keys loaded successfully.")
+print("version", version) #status
+print("Keys loaded successfully.") #status
 
 auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_SECRET"))
 auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
 
-api = tweepy.API(auth)
-print("Authenticated with OAuth Twitter API.")
+print("Authenticating with OAuth...") #status
 
-print("Searching for tweets...")
+api = tweepy.API(auth)
+print("Authenticated with OAuth Twitter API.") #status
+
+print("Searching for tweets...") #status
 tweets = tweepy.Cursor(api.search_tweets, q=hashtag).items(number_of_tweets)
-print("Fetched ", number_of_tweets," tweets.")
+print("Fetched ", number_of_tweets," tweets.") #status
 
 
 locations = {}
 
+print("Loading locations dictionary...") # status
 for tweet in tweets:
     location = tweet.user.location
     if location:
@@ -35,8 +38,35 @@ for tweet in tweets:
         else:
             locations[location] = 1
 
-print("Locations dictionary loaded successfully.")
+print("Locations dictionary loaded successfully.") #status
 
+print("Using geolocator to obtain Latitudes and Longitudes of user locations...") #status
+geolocator = Nominatim(user_agent="my_application") 
+latitudes = []
+longitudes = []
+counts = []
+print("Starting geolocator...") #status
+for location in locations:
+    try:
+        location_info = geolocator.geocode(location)
+        if location_info:
+            latitudes.append(location_info.latitude)
+            longitudes.append(location_info.longitude)
+            counts.append(locations[location])
+    except:
+        pass
+print("Geolocator coordinates obtained successfully.") #status
+
+print("Creating ScatterPlot...") #status
+plt.scatter(longitudes, latitudes, s=counts, alpha=0.5)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("Locations of Tweets on #flutter")
+print("Saving ScatterPlot...") #status
+plt.savefig("locations_scatterplot.png")
+plt.show()
+
+print("Creating BarPlot...") #status
 fig, ax = plt.subplots()
 ax.bar(locations.keys(), locations.values())
 plt.xticks(rotation=90)
@@ -44,6 +74,7 @@ plt.xlabel("Location")
 plt.ylabel("Number of Tweets")
 plt.title("Locations of Tweets on #flutter")
 plt.tight_layout()
-plt.savefig("locations.png")
+print("Saving BarPlot...") #status
+plt.savefig("locations_barplot.png")
 plt.show()
 print("Finished.")
