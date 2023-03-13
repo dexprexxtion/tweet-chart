0153a3fdc8895fa579bcdf5c040e17459348e026
Ketan Chowdhury <ketanchowdhury@protonmail.com>
2023-03-13 19:05:13 +0530

    Scatter plot made to show tweet location data

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tweepy, os
+import tweepy
+import os
+import geopy
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
+from geopy.geocoders import Nominatim
 
 load_dotenv()
 
@@ -8,14 +12,35 @@ auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"
 
 api = tweepy.API(auth)
 
-tweets = tweepy.Cursor(api.search_tweets, q="#flutter").items(10)
+tweets = tweepy.Cursor(api.search_tweets, q="#flutter").items(100)
+
+locations = {}
 
 for tweet in tweets:
-    print("User: ", tweet.user.screen_name)
-    print("Text: ", tweet.text)
-    print("Retweets: ", tweet.retweet_count)
-    print("Favorites: ", tweet.favorite_count)
-    print("Created: ", tweet.created_at)
-    print("Location: ", tweet.user.location)
-    print("Source: ", tweet.source)
-    print("\n")
+    location = tweet.user.location
+    if location:
+        if location in locations:
+            locations[location] += 1
+        else:
+            locations[location] = 1
+
+geolocator = Nominatim(user_agent="tweet_chart")
+latitudes = []
+longitudes = []
+counts = []
+for location in locations:
+    try:
+        location_info = geolocator.geocode(location)
+        if location_info:
+            latitudes.append(location_info.latitude)
+            longitudes.append(location_info.longitude)
+            counts.append(locations[location])
+    except:
+        pass
+
+plt.scatter(longitudes, latitudes, s=counts, alpha=0.5)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("Locations of Tweets on #flutter")
+plt.savefig("locations.png")
+plt.show()  
