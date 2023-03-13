0d3032e30e813966a192686a7b03c913bad8f1f6
Ketan Chowdhury <ketanchowdhury@protonmail.com>
2023-03-13 19:14:38 +0530

    Print statements added to show progress.

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 from geopy.geocoders import Nominatim
 
+version = 0.1
+number_of_tweets = 100
+
 load_dotenv()
+
+print("version", version)
+print("Keys loaded successfully.")
 
 auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_SECRET"))
 auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
 
 api = tweepy.API(auth)
+print("Authenticated with OAuth Twitter API.")
 
-tweets = tweepy.Cursor(api.search_tweets, q="#flutter").items(100)
+print("Searching for tweets...")
+tweets = tweepy.Cursor(api.search_tweets, q="#flutter").items(number_of_tweets)
+print("Fetched ", number_of_tweets," tweets.")
+
 
 locations = {}
 
@@ -23,6 +33,8 @@ for tweet in tweets:
             locations[location] += 1
         else:
             locations[location] = 1
+
+print("Locations dictionary loaded successfully.")
 
 geolocator = Nominatim(user_agent="tweet_chart")
 latitudes = []
@@ -38,9 +50,14 @@ for location in locations:
     except:
         pass
 
+print("Geolocator job done.")
+print("Creating ScatterPlot")
+
 plt.scatter(longitudes, latitudes, s=counts, alpha=0.5)
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.title("Locations of Tweets on #flutter")
 plt.savefig("locations.png")
 plt.show()  
+
+print("Finished.")
